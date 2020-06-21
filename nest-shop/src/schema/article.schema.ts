34a919a1078fef37b96cde1ebecc047ba3ab036a
刘思鑫 <fordreamxkhl@163.com>
2020-06-22 00:43:32 +0800

    chore: rm shop

--- nest-shop/src/schema/article.schema.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from 'mongoose';
-
-export const ArticleSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  keywords: String,
-  content: String,
-  status: Number,
-});
--- nest-shop/src/schema/nav.schema.ts
+++
@@ -1,7 +0,0 @@
-import * as mongoose from 'mongoose';
-
-export const NavSchema = new mongoose.Schema({
-  title: String,
-  url: String,
-  status: Number,
-});
